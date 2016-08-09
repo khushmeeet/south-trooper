701327a202da05a0cda24c84312909c86900e526
Khushmeet <khushmeetsingh1996@gmail.com>
2016-08-09 22:10:19 +0530

    fixes

--- karma.conf.js
+++ karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
       'client/bower_components/angular-aria/angular-aria.js',
       'client/bower_components/angular-messages/angular-messages.js',
       'client/bower_components/angular-material/angular-material.js',
+      'client/bower_components/ng-file-upload/ng-file-upload.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'node_modules/socket.io-client/socket.io.js',
